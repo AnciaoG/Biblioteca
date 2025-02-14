5f25b5cdf59ad99a3d22c6be03299b924e324d1e
AnciaoG <gabrielandrade@acad.ifma.edu.br>
2025-02-14 18:12:28 -0300

    criaçãoo do principal

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
 import express from 'express';
 const app = express();
+import { engine } from 'express-handlebars';
+
+
+/**CONFIGURAÇÃO DA VISÃO */
+app.engine('handlebars', engine({ defaultLayout: 'Principal' })); 
+app.set('view engine', 'handlebars');
+
+/**ROTAS DO SISTEMA */
+app.get('/', function(req, res){
+    res.render('layouts/Principal')
+})
 
 // Inicia o servidor
 app.listen(2200, () => {
